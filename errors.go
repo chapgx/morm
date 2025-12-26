ef26311d1da3261227c6e4c4d92e7cc6b09b5fff
Richard Chapman Gomez <chapgx@codeh.io>
2025-12-25 22:26:30 -0500

    Expanded On Error
     Added internal error wrapper function and added more verbose panic on
     insert

--- errors.go
+++ errors.go
@@ -1,5 +1,14 @@
 package morm
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var ErrValIsNotExpectedType = errors.New("val is not of the expected type")
+
+// err_wrap wraps an error with a message
+func err_wrap(e error, msg string) error {
+	e = fmt.Errorf("%s => %s", e.Error(), msg)
+	return e
+}
